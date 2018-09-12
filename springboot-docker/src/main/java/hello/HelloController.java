b54fdc55b2117fcdc340b16e57383870f4452500
xushipeng <xushipeng@meizu.com>
2018-09-12 15:35:51 +0800

    add docker demo

---
+++ springboot-docker/src/main/java/hello/HelloController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Docker!";
+    }
+}
